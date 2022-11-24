0e9e92f5a2697adceb91b12a783a40d60631feca
Corey <cwills788@gmail.com>
2022-11-24 11:05:40 -0600

    added more jobs to jobsData

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import { useState } from 'react'
+import { jobsData } from './JobsData'
 import './App.css';
 import JobList from './JobList';
 
-const jobList = [
-  {
-    jobRank: 1,
-    jobTitle: 'Nurse practitioners',
-    growthRate: 46,
-    medianSalary: '$120,680',
-    jobImage: 'https://www.stkate.edu/sites/default/files/2022-07/nurse_practitioner_pediatric.jpg'
-  },
-  {
-    jobRank: 2,
-    jobTitle: 'Wind turbine service technicians',
-    growthRate: 44,
-    medianSalary: '$56,260',
-    jobImage: 'https://skillpointe.com/sites/default/files/styles/1200_x_530_banner/public/2020-08/Wind%20turbine%20technician.jpg?itok=OGmQ-t3y'
-  },
-  {
-    jobRank: 3,
-    jobTitle: 'Ushers, lobby attendants, and ticket takers',
-    growthRate: 41,
-    medianSalary: '$24,440',
-    jobImage: 'https://images.forbes.com/media/2007/06/04/worstpaid_10.jpg'
-  },
-  {
-    jobRank: 4,
-    jobTitle: 'Motion picture projectionists',
-    growthRate: 40,
-    medianSalary: '$29,350',
-    jobImage: 'https://www.owlguru.com/wp-content/uploads/wpallimport/files/motion-picture-projectionists/__(1).jpg'
-  },
-  {
-    jobRank: 5,
-    jobTitle: 'Cooks, restaurant',
-    growthRate: 37,
-    medianSalary: '$30,010',
-    jobImage: 'https://blog.chefworks.com/uniforms/wp-content/uploads/2015/12/image001.jpg'
-  }
-]
-
 function App() {
-  const [jobs, setJobs] = useState(jobList)
+  const [jobs, setJobs] = useState(jobsData)
   const [searchInput, setSearchInput] = useState('')
 
   const searchInputChangeHandler = (e) => {
@@ -53,19 +16,19 @@ function App() {
   return (
     <div className="App">
       <h1>Fastest Growing Occupations</h1>
+      <p>(according to BLS)</p>
       <input 
         placeholder='Search jobs' 
         value={searchInput}
         onChange={searchInputChangeHandler}
       />
-      <p>(according to BLS)</p>
       <ol style={{listStyle: 'none', paddingLeft: '0px'}}>
         {
           showJobs.map(job => {
             return (
               <li key={job.jobRank}>
                 <h3>{job.jobRank}. {job.jobTitle}</h3>
-                <img className='thumbnail' src={job.jobImage} />
+                <img className='thumbnail' src={job.jobImage} alt={job.jobTitle} />
                 <h5>Growth Rate: {job.growthRate}%</h5>
                 <h6>Median Salary {job.medianSalary}</h6>
               </li>
---
+++ src/JobsData.js
@@ -0,0 +1,72 @@
+export const jobsData = [
+    {
+      jobRank: 1,
+      jobTitle: 'Nurse practitioners',
+      growthRate: 46,
+      medianSalary: '$120,680',
+      jobImage: 'https://www.stkate.edu/sites/default/files/2022-07/nurse_practitioner_pediatric.jpg'
+    },
+    {
+      jobRank: 2,
+      jobTitle: 'Wind turbine service technicians',
+      growthRate: 44,
+      medianSalary: '$56,260',
+      jobImage: 'https://skillpointe.com/sites/default/files/styles/1200_x_530_banner/public/2020-08/Wind%20turbine%20technician.jpg?itok=OGmQ-t3y'
+    },
+    {
+      jobRank: 3,
+      jobTitle: 'Ushers, lobby attendants, and ticket takers',
+      growthRate: 41,
+      medianSalary: '$24,440',
+      jobImage: 'https://images.forbes.com/media/2007/06/04/worstpaid_10.jpg'
+    },
+    {
+      jobRank: 4,
+      jobTitle: 'Motion picture projectionists',
+      growthRate: 40,
+      medianSalary: '$29,350',
+      jobImage: 'https://www.owlguru.com/wp-content/uploads/wpallimport/files/motion-picture-projectionists/__(1).jpg'
+    },
+    {
+      jobRank: 5,
+      jobTitle: 'Cooks, restaurant',
+      growthRate: 37,
+      medianSalary: '$30,010',
+      jobImage: 'https://blog.chefworks.com/uniforms/wp-content/uploads/2015/12/image001.jpg'
+    },
+    {
+      jobRank: 6,
+      jobTitle: 'Data scientists',
+      growthRate: 36,
+      medianSalary: '$109,910',
+      jobImage: 'https://www.bls.gov/ooh/images/96312.png'
+    },
+    {
+      jobRank: 7,
+      jobTitle: 'Athletes and sports competitors',
+      growthRate: 36,
+      medianSalary: '$77,300',
+      jobImage: 'https://thumbor.forbes.com/thumbor/740x0/smart/https%3A%2F%2Fimages.forbes.com%2Fhighest-paid-athletes-package-2022%2Flander-header.jpg'
+    },
+    {
+      jobRank: 8,
+      jobTitle: 'Information security analysts',
+      growthRate: 35,
+      medianSalary: '$102,600',
+      jobImage: 'https://www.wgu.edu/content/dam/web-sites/blog-newsroom/blog/images/national/2019/april/threeITmoviecharacters.jpg'
+    },
+    {
+      jobRank: 9,
+      jobTitle: 'Statisticians',
+      growthRate: 33,
+      medianSalary: '$95,570',
+      jobImage: 'https://www.ziprecruiter.com/svc/fotomat/public-ziprecruiter/cms/912637018QuantitativeAnalyst.jpg'
+    },
+    {
+      jobRank: 10,
+      jobTitle: 'Umpires, referees, and other sports officials',
+      growthRate: 32,
+      medianSalary: '$35,860',
+      jobImage: 'https://sirc.ca/wp-content/uploads/2019/07/fotolia_107611917.jpg'
+    }
+  ]
